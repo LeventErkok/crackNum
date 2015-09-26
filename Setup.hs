22292819f20d2edff76cd4fe97fa75afbf2e4bd7
Levent Erkok <erkokl@gmail.com>
2015-09-26 16:20:20 -0700

    Upgrade to new travis infrastructure

---
+++ Setup.hs
@@ -0,0 +1,18 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Main
+-- Copyright   :  (c) Levent Erkok
+-- License     :  BSD3
+-- Maintainer  :  erkokl@gmail.com
+-- Stability   :  experimental
+--
+-- Setup module for crackNum
+-----------------------------------------------------------------------------
+
+{-# OPTIONS_GHC -Wall #-}
+module Main(main) where
+
+import Distribution.Simple
+
+main :: IO ()
+main = defaultMain
